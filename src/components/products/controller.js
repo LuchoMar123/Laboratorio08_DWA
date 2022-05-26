6c93d4b91bf6e1647eeb8ebb61311d6ea4d03ffd
LuchoMar123 <you@example.com>
2022-05-25 21:52:49 -0500

    test action

--- src/components/products/controller.js
+++ src/components/products/controller.js
@@ -5,6 +5,6 @@
 export const getProducts = (req, res) => {
   res.status(200).json({
     ok: true,
-    body: "Testing ci/cd whit Docker Luis Marina",
+    body: "Testing ci/cd with Docker Luis Marina",
   });
 };
